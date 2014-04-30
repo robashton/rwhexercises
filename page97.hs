04e9d1ec11bc7c64f4cf0b3a564a99a5365f90f4
Rob Ashton <robashton@codeofrob.com>
2014-04-30 17:46:47 +0100

    More exercises, need to work out why foldr short circuits or not

--- page97.hs
+++ page97.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import Debug.Trace
 
 
 loop :: Int -> String -> Int
@@ -9,9 +10,26 @@ loop acc [] = acc
 asInt :: String -> Int
 asInt xs = loop 0 xs
 
+-- The rest of this exercise has broken text and I cba
 asInt_fold :: String -> Int
+asInt_fold ('-':xs) = -(asInt_fold xs)
+asInt_fold [] = 0
 asInt_fold xs = foldl parseInt 0 xs
-   where parseInt acc x | 0 '-' = 0 :: Int
-                        | acc x = (acc * 10) + (digitToInt x)
+   where parseInt acc x = acc * 10 + digitToInt x
 
+myconcat :: [[a]] -> [a]
+myconcat xs = foldr step [] xs
+  where  step ys x = ys ++ x
 
+-- Ooh, we can use fns in guard clauses because purity
+mytakewhilerec :: (a -> Bool) -> [a] -> [a]
+mytakewhilerec fn xs = step xs []
+  where step [] ys = ys
+        step (y:rest) ys | fn y = step rest (ys ++ [y])
+                         | otherwise = ys
+
+-- Returning an empty list
+mytakewhile :: (a -> Bool) -> [a] -> [a]
+mytakewhile fn coll = foldr step [] coll
+   where step x ys | fn x = x : ys
+                   | otherwise = [x]
