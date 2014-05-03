31b56d701f5035526fe186502a41fca81d2cc0c7
Rob Ashton <robashton@codeofrob.com>
2014-05-03 10:46:20 +0100

    PutJson

--- PutJson.hs
+++ PutJson.hs
@@ -3,3 +3,22 @@ module PutJson where
 import Data.List (intercalate)
 import Json
 
+renderJValue :: JValue -> String
+renderJValue (JString s) = show s
+renderJValue (JNumber s) = show s
+renderJValue (JBool True) = "true"
+renderJValue (JBool False) = "false"
+renderJValue JNull = "null"
+
+renderJValue (JObject o) = "{" ++ pairs o ++ "}"
+  where pairs [] = ""
+        pairs ps = intercalate ",\n" (map renderPair ps)
+        renderPair (k,v) = show k ++ ": " ++ renderJValue v
+
+renderJValue (JArray a) = "[" ++ values a ++ "]"
+  where values [] = ""
+        values vs = intercalate ", " (map renderJValue vs)
+
+
+putJValue :: JValue -> IO()
+putJValue v = putStrLn (renderJValue v)
